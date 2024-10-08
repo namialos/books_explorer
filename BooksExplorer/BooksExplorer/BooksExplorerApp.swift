87ac9d9178e31a71553072cfab51a00303af7aa5
Babak Baradari <babak.baradari93@gmail.com>
2024-10-08 23:37:12 +0330

    Initial commit

---
+++ BooksExplorer/BooksExplorer/BooksExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksExplorerApp.swift
+//  BooksExplorer
+//
+//  Created by babak on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct BooksExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
